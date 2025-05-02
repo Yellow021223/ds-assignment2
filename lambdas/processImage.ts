ddc3edc0bec58cbacfa9ad674e8761a6f3957f8c
dong <2389314853@qq.com>
2025-05-02 18:24:53 +0100

    update processImage

--- lambdas/processImage.ts
+++ lambdas/processImage.ts
@@ -20,7 +20,7 @@ export const handler: SQSHandler = async (event) => {
     const s3Info = body.Records?.[0]?.s3;
 
     if (!s3Info) {
-      console.log("No S3 info in message");
+      console.log("No message");
       continue;
     }
 
@@ -28,8 +28,8 @@ export const handler: SQSHandler = async (event) => {
     const ext = objectKey.slice(objectKey.lastIndexOf(".")).toLowerCase();
 
     if (!allowedExtensions.includes(ext)) {
-      console.log(`Unsupported file type: ${ext}`);
-      throw new Error("Unsupported file type");
+      console.log(`Fail to run file type: ${ext}`);
+      throw new Error("Fail to run file type");
     }
 
     const putCommand = new PutItemCommand({
